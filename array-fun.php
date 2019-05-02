341ff97fd7456db3a268d5875f65123d0007fb6c
attrigirish <41194400+attrigirish@users.noreply.github.com>
2019-05-02 16:13:17 +0530

    Add files via upload

---
+++ array-fun.php
@@ -0,0 +1,23 @@
+<?php
+
+//Inserting an Element at the beginning
+$arr=array(43,65,34,76);
+array_unshift($arr,99);
+print_r($arr);
+echo "<br>";
+
+
+//Deleting an Element from the beginning
+$arr=array(43,65,34,76);
+array_shift($arr);
+print_r($arr);
+echo "<br>";
+
+
+//Inserting an Element at the end of an array
+$arr=array(43,65,34,76);
+array_unshift($arr,99);
+print_r($arr);
+
+
+?>
